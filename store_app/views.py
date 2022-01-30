015587a588d7edcbe65e1426aa012c3b59d9117f
Cetin OGUT <cetinogut.gm@gmail.com>
2022-01-30 10:07:41 +0300

    Finished the templates initial design

--- store_app/views.py
+++ store_app/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def store(request):
 	context = {}
-	return render(request, 'store/store.html', context)
+	return render(request, 'store_app/store.html', context)
 
 def cart(request):
 	context = {}
-	return render(request, 'store/cart.html', context)
+	return render(request, 'store_app/cart.html', context)
 
 def checkout(request):
 	context = {}
-	return render(request, 'store/checkout.html', context)
+	return render(request, 'store_app/checkout.html', context)
